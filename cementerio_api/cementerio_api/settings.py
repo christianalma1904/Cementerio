31efe7a4006e5dfb63274029acd1339d3069c1d4
Daniel Alcocer <christianalma001@gmail.com>
2026-01-05 16:49:33 -0500

    Update

--- cementerio_api/cementerio_api/settings.py
+++ cementerio_api/cementerio_api/settings.py
@@ -178,12 +178,34 @@ SECURE_HSTS_PRELOAD = False
 # CORS Configuration
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+    "http://localhost:3001",
+    "http://localhost:3002",
+    "http://localhost:3003",
+    "http://localhost:5000",
     "http://localhost:8000",
-    "http://127.0.0.1:8000",
     "http://127.0.0.1:3000",
+    "http://127.0.0.1:3001",
+    "http://127.0.0.1:3002",
+    "http://127.0.0.1:3003",
+    "http://127.0.0.1:5000",
+    "http://127.0.0.1:8000",
     "http://localhost",
     "http://127.0.0.1",
+    "http://[::1]:3000",
+    "http://[::1]:3001",
+    "http://[::1]:8000",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
