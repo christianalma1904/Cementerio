022bf4801ac7b4022ededd86264c1b9b59135d73
Daniel Alcocer <christianalma001@gmail.com>
2026-01-14 21:14:02 -0500

    settings.py Update

--- cementerio_api/cementerio_api/settings.py
+++ cementerio_api/cementerio_api/settings.py
@@ -194,6 +194,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://[::1]:3000",
     "http://[::1]:3001",
     "http://[::1]:8000",
+    "http://alcocer-billing-ui.desarrollo-software.xyz",
+    "http://192.188.51.84",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
